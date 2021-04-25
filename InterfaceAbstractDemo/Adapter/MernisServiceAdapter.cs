930de9348e81e42d925d6d1aa91441ba21d6e425
10urb <10urbulut@gmail.com>
2021-04-25 19:09:41 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Adapter/MernisServiceAdapter.cs
@@ -0,0 +1,21 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using System;
+using MernisServiceReference;
+
+namespace InterfaceAbstractDemo.Adapter
+{
+    public class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+         KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            
+            return client.TCKimlikNoDogrula(Convert.ToInt64(customer.NationalityId),
+                                                 customer.FirstName.ToUpper(),
+                                                 customer.LastName.ToUpper(),
+                                                 customer.DateOfBirth.Year);
+            
+        }
+    }
+}
